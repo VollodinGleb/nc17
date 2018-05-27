003a4c237dfd65b49425d364eb921d1ef21723f8
mashianov <amashianov@admins-MacBook-Pro.local>
2018-05-27 13:08:01 +0300

    Asd

--- conf/settings.py
+++ conf/settings.py
@@ -140,10 +140,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
 )
